637e04fb0aea72eab8b01416ed5ab0b83f34be5a
Dennis Magnusson <dennis.magnusson@aalto.fi>
2022-06-28 20:46:17 +0300

    4.2 completed

---
+++ app.js
@@ -0,0 +1,23 @@
+const config = require('./utils/config.js')
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs.js')
+const mongoose = require('mongoose')
+
+console.log('connecting to', config.MONGODB_URI)
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
