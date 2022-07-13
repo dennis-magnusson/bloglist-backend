e56a7346c0f8055f52bb2c64111772b31317a8f5
Dennis Magnusson <dennis.magnusson@aalto.fi>
2022-07-13 15:16:59 +0300

    4.18 completed

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs.js')
 const usersRouter = require('./controllers/users.js')
+const loginRouter = require('./controllers/login.js')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger.js')
 
@@ -25,6 +26,7 @@ app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
